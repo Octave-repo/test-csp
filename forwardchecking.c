7f6e1e91162943cbe12b70571a5f8fe520250c0d
Octave-portfolio <valadieroctave@gmail.com>
2023-12-12 14:17:49 +0100

    Removed comments

--- forwardchecking.c
+++ forwardchecking.c
@@ -47,7 +47,6 @@ int is_ok(int a, int b, Couple *contrainte, int size)
     for (int j = 0; j < size; j++)
     {
         Couple c = contrainte[j];
-        //printf("Test du couple %d : %d\n", a, b);   
         if (c.a == a && c.b == b && c.a != NONEVALUE && c.b != NONEVALUE)
         {
             return 1;
@@ -55,42 +54,6 @@ int is_ok(int a, int b, Couple *contrainte, int size)
     }
     return 0;
 }
-
-//On révise n
-/*int **revise(int m, int n, int **definitions, int nb_var, int nb_val, Couple ***csp, int size) {
-    //n >= m
-    //La contrainte entre n et m
-    Couple *contrainte = csp[n][m];
-    //Pour simplifier plus tard
-    int *definition_m = definitions[m];
-    int *definition_n = definitions[n];
-
-    //Si on a pas de contrainte, inutile de réviser 
-    if (!contrainte)
-        return definitions;
-        
-    //Pour a ∈ Dm
-    for (int i = 0 ; i < nb_val ; i++)
-    {
-        int ok = 0;
-        // Si il existe une valeur dans Dn inconsistnente
-        for (int j = 0 ; j < nb_val ; j++)
-        {           
-            if (is_ok(definition_n[j], definition_m[i],contrainte, size))
-            {
-                printf("La valeur: %d est consistente.\n", definition_m[i]);
-                ok = 1;
-                break;
-            }
-        }
-        if (!ok)
-        {
-            printf("Je supprime la valeur : %d\n", definition_m[i]);
-            definition_m[i] = -1;
-        }
-    }
-    return definitions;
-}*/
 
 int *remove_value_from_domain(int *domain, int value, int size) {
  
@@ -145,15 +108,6 @@ void restore_domains(int **source, int **destination, int start, int end, int nb
     }
 }
 
-//TODO Clean up
-/*void restore_domains(int **source, int **destination, int start, int end, int nb_var, int nb_val)
-{
-    for (int i = start; i < nb_var; i++) {
-        for (int j = 0; j < nb_val; j++) {
-            destination[i][j] = j;
-        }
-    }
-}*/
 int *forwardcheck(int nb_var, int nb_val, int durete, Couple ***csp)
 {
     int *out;
@@ -184,7 +138,6 @@ int *forwardcheck(int nb_var, int nb_val, int durete, Couple ***csp)
             // // Di* <- Di* - {x}
             int empty_domain = 0;
             for (int k = i+1; k < nb_var; k++) {
-                //definitions = revise(k, i, definitions, nb_var, nb_val, csp, size);
                 definitions = revise(k, i, x,definitions, nb_var, nb_val, csp, size);
                 if (is_empty(definitions[k], nb_val)) {
                     empty_domain = 1;
@@ -196,20 +149,10 @@ int *forwardcheck(int nb_var, int nb_val, int durete, Couple ***csp)
                 ok = 1;
             } else
             {
-                //restore_domains(definitions_copy, definitions, i + 1, nb_var, nb_var, nb_val);
                 copy_domain(definitions_copy, definitions, nb_var, nb_val);
             }
         }
         if (!ok) {
-            //Enfait peut être pas besoin de faire une backup ?
-            //TODO Changer un truc ici :(
-            /*for (int a = i ; a < nb_var ; a++){
-                for (int b = 0 ; b < nb_val ; b++){
-                    definitions[a][b] = b;
-                }
-            }*/
-            //TESTER ICI
-            //restore_domains(definitions_copy, definitions, i +1, nb_var, nb_var, nb_val);
             i--;
             if (i >= 0)
                 copy_domain(last_valid_definitions[i], definitions, nb_var, nb_val);
@@ -218,7 +161,6 @@ int *forwardcheck(int nb_var, int nb_val, int durete, Couple ***csp)
         }
     }
     end_time = clock();
-    
     execution_time = (double)(end_time - start_time) / CLOCKS_PER_SEC;
     printf("Temps d'éxecution: %.6f secondes\n", execution_time);
     //Si i = -1 alors pas de solution (NULL)
